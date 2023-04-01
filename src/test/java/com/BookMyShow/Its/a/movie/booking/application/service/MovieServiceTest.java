27006f70cc4cefc4c09f1562a2bcbc4edd84c625
soumyadeep2912 <rsoumyadeep5@gmail.com>
2023-04-01 17:02:58 +0530

    Unit Testing of MovieService Class

--- src/test/java/com/BookMyShow/Its/a/movie/booking/application/service/MovieServiceTest.java
+++ src/test/java/com/BookMyShow/Its/a/movie/booking/application/service/MovieServiceTest.java
@@ -13,35 +13,28 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class MovieServiceTest {
     @Autowired
     private MovieService movieService;
-   private static Movie movie;
-//   @BeforeEach
-//  public void beforeEach(){
-//   movie=new Movie();
-// movie.setMovieName("Pathan");
-//  movie.setMovieDesc("Good");
-//  movie.setRating(10);
-//  movie.setDuration(280.0);
-//   movie.setCoverPhtUrl("www.coverphtUrl.com");
-//  movie.setTrailerUrl("www.trailerUrl.com");
-//
-//    }
+   private Movie movie;
+   @BeforeEach
+  public void beforeEach(){
+   movie=new Movie();
+ movie.setMovieName("Pathan");
+  movie.setMovieDesc("Good");
+  movie.setRating(10);
+  movie.setDuration(280.0);
+   movie.setCoverPhtUrl("www.coverphtUrl.com");
+  movie.setTrailerUrl("www.trailerUrl.com");
 
-//    @Test
-//    public void testCreateMovie(){
-//        movie=new Movie();
-//        movie.setMovieId("1");
-//        movie.setMovieName("Pathan");
-//        movie.setMovieDesc("Good");
-//        movie.setRating(10);
-//        movie.setDuration(280.0);
-//        movie.setCoverPhtUrl("www.coverphtUrl.com");
-//        movie.setTrailerUrl("www.trailerUrl.com");
-//      Movie actualMovieObject=movieService.createMovie(movie);
-//       Assertions.assertNotNull(actualMovieObject);
-//      Assertions.assertEquals(0,actualMovieObject.getMovieId());
-//
-//        System.out.println(movieService);
-//    }
+    }
+
+    @Test
+    public void testCreateMovie(){
+
+      Movie actualMovieObject=movieService.createMovie(movie);
+       Assertions.assertNotNull(actualMovieObject);
+      Assertions.assertEquals("642815f444b8c27b8af31722",actualMovieObject.getMovieId());
+
+        System.out.println(movieService);
+    }
     @Test
     public void testGetMovieDetails() throws MovieDetailsNotFoundException {
         Movie savedMovie=movieService.getMovieBasedOnId("640c48ffd4656b7352640470");
---
+++ src/test/java/com/BookMyShow/Its/a/movie/booking/application/service/MovieServiceUnitTest.java
@@ -0,0 +1,56 @@
+package com.BookMyShow.Its.a.movie.booking.application.service;
+
+import com.BookMyShow.Its.a.movie.booking.application.model.Movie;
+import com.BookMyShow.Its.a.movie.booking.application.repository.MovieRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MovieServiceUnitTest {
+    @Mock
+    MovieRepository movieRepository;
+
+    @InjectMocks
+   private MovieService movieService;
+    private Movie movie;
+    private Movie savedMovie;
+
+    @BeforeEach
+    public void beforeEach(){
+                movie=new Movie();
+                movie.setMovieName("Pathan");
+                movie.setMovieDesc("Good");
+                movie.setRating(10);
+                movie.setDuration(280.0);
+                movie.setCoverPhtUrl("www.coverphtUrl.com");
+                movie.setTrailerUrl("www.trailerUrl.com");
+
+                savedMovie=new Movie();
+                savedMovie.setMovieId("0");
+                savedMovie.setMovieName("Pathan");
+                savedMovie.setMovieDesc("Good");
+                savedMovie.setRating(10);
+               savedMovie.setDuration(280.0);
+               savedMovie.setCoverPhtUrl("www.coverphtUrl.com");
+               savedMovie.setTrailerUrl("www.trailerUrl.com");
+
+
+
+
+    }
+    @Test
+    public void testMovieDetails(){
+        Mockito.when(movieRepository.save(movie)).thenReturn(savedMovie);
+        Movie savedMovie = movieService.createMovie(movie);
+        Assertions.assertNotNull(savedMovie);
+        Assertions.assertEquals("0",savedMovie.getMovieId());
+
+       // System.out.println(movieService);
+    }
+
+}
